ac6115e898f980a2109e812bac90434159cfd4ec
KoffeMeow <guyden999@mail.ru>
2022-08-04 16:13:04 +0300

    fix style

--- pages/api/tabs.tsx
+++ pages/api/tabs.tsx
@@ -7,67 +7,78 @@ import Password from "./password"
 import QuickResponse from './qrcode';
 
 const calculator = (
-	<div className="body" key={Date.now()}>
-            <Head>
-				<title>Сalculator</title>
-				<meta name="description" content="" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+	<>
+		<Head>
+			<title>Сalculator</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
 
 		<h1>Calculator</h1>
-
-		<div className="calculator">
+		<div className="body" key={Date.now()}>
 			<Calculator />
 		</div>
-	</div>
+	</>
 );
 
 const stopwatch = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>Stopwatch</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+	<>
+		<Head>
+			<title>Stopwatch</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
 		<h1>Stopwatch</h1>
-		<Stopwatch/>
-	</div>
+		<div className="body" key={Date.now()}>
+			<Stopwatch/>
+		</div>
+	</>
 );
 
 const countdown = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>Countdown</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+	<>
+		<Head>
+			<title>Countdown</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
 		<h1>New Year Countdown</h1>
-		<Countdown/>
-	</div>
+		<div className="body" key={Date.now()}>
+			<Countdown/>
+		</div>
+	</>
 );
 
 const passwordGenerator = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>Password generator</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+	<>
+		<Head>
+			<title>Password generator</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
 		<h1>Password generator</h1>
-		<Password/>
-	</div>
+		<div className="body" key={Date.now()}>
+			<Password/>
+		</div>
+	</>
 );
 
 const QRcodeGenerator = (
-	<div className="body" key={Date.now()}>
-        <Head>
-            <title>QR-code generator</title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+	<>
+		<Head>
+			<title>QR-code generator</title>
+			<meta name="description" content="" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
 		<h1>QR-code generator</h1>
-		<QuickResponse/>
-	</div>
+		<div className="body" key={Date.now()}>
+			<QuickResponse/>
+		</div>
+	</>
 );
 
 export const tabs = [
