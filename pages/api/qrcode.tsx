4d058396fb493f3eb3cac27b2cb3a6e1a5d80dfd
KoffeMeow <guyden999@mail.ru>
2022-08-20 21:15:36 +0300

    qr code fix

--- pages/api/qrcode.tsx
+++ pages/api/qrcode.tsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import QRCode from 'react-qr-code';
 
 const QuickResponse = () => {   
-
+    
     const [inputValue, setInputValue] = useState('');
+
+    const handleChange = event => {
+        const result = event.target.value.replace(/[ЁёА-я]/, '');
+    
+        setInputValue(result);
+    };
 
     return (
         <div className="qrcode__wrap">
             <div className="qrcode"><QRCode value={inputValue}></QRCode></div>
             <p>* This generator works with links, symbols and letters of the English alphabet.</p>  
-            <input type="text" placeholder="Enter text or link" onChange={e => setInputValue(e.target.value)} />
+            <input type="text" value={inputValue} placeholder="Enter text or link" onChange={handleChange}/>
         </div>
+
+        
     )
 }
 
